19be6bbed7d66bf972793ba7aaf888ce68b70d3d
Aimilios <aim.zacharakis@gmail.com>
2026-04-26 16:42:37 +0300

    Fixed User Registration

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        # Load signals so UserProfile is auto-created on user registration
+        import accounts.models
